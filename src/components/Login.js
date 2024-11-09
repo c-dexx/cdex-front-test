be44ea43194576584ccae53253145c53c00fa80c
ramaprawira <ramaprawira727@gmail.com>
2024-11-09 18:12:29 +0700

    fix: login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -13,27 +13,32 @@ const Login = () => {
   };
 
   return (
-    <Box maxW="sm" mx="auto" p="8" mt="10" bg="gray.50" shadow="md" rounded="md">
-      <Heading as="h1" mb="6">Login</Heading>
-      <Input
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        mb="4"
-      />
-      <Input
-        placeholder="Password"
-        value={password}
-        type="password"
-        onChange={(e) => setPassword(e.target.value)}
-        mb="6"
-      />
-      <Button colorScheme="blue" w="full" onClick={handleSubmit}>
-        Login
-      </Button>
-      <Text mt="4" textAlign="center">
-        Don't have an account? <Link to="/signup">Sign Up</Link>
-      </Text>
+    <Box
+    bgGradient="linear(to-r, gray.700, gray.900)"
+    minH="100vh"
+    overflow={'hidden'}>
+      <Box maxW="sm" mx="auto" p="8" bg="gray.50" shadow="md" rounded="md" mt="72">
+        <Heading as="h1" mb="6">Login</Heading>
+        <Input
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          mb="4"
+        />
+        <Input
+          placeholder="Password"
+          value={password}
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          mb="6"
+        />
+        <Button colorScheme="blue" w="full" onClick={handleSubmit}>
+          Login
+        </Button>
+        <Text mt="4" textAlign="center">
+          Don't have an account? <Link to="/signup">Sign Up</Link>
+        </Text>
+      </Box>
     </Box>
   );
 };
